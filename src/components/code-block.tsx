a7a7b454d13c3904e43f6f31a271f893a0ece201
Sami TAAISSAT <sami.taaissat@artefact.com>
2025-01-26 18:16:10 +0100

    feat: mermaid and code

---
+++ src/components/code-block.tsx
@@ -0,0 +1,15 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomOneDark from 'react-syntax-highlighter/dist/cjs/styles/prism/material-dark';
+
+const Codeblock = ({ children, language }: { children: string, language: string }) => {
+  return (
+    <SyntaxHighlighter
+      language={language || 'jsx'}
+      style={atomOneDark}
+      >
+          {children}
+    </SyntaxHighlighter>
+  );
+};
+
+export { Codeblock };
