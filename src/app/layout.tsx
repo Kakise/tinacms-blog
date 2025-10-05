478aee220d82d0aaa0f66da422c00426875fa3c2
Kakise <plaid@a-smol-cat.fr>
2025-10-05 05:33:02 +0200

    fix: no analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Nav from '@/components/nav'
 import { ViewTransitions } from 'next-view-transitions'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Space_Mono } from 'next/font/google'
 import React from 'react'
 
@@ -22,8 +20,6 @@ export default function RootLayout({
     <ViewTransitions>
       <html lang="en">
         <body className={space_mono.className}>
-          <Analytics />
-          <SpeedInsights />
           <Nav />
           <div className="dark:text-darkText text-text mx-auto w-[750px] max-w-full px-5 pt-28 pb-10">
             {children}
